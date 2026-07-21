37bb267159eec9c901cac09ba8d8601d22892bbf
Lakshya Surana <lakshyasurana101@gmail.com>
2026-07-21 17:54:00 +0530

    Add booking HTML form template and update view route

--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,10 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def index(request):
+#     return HttpResponse("<h1>Welcome to the Little Lemon Booking Page!</h1>")
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Welcome to the Little Lemon Booking Page!</h1>")
+    # This renders your booking.html file instead of raw text
+    return render(request, 'booking.html')
