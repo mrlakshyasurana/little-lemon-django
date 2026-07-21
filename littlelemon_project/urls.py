8135742c96f79372c4adaf058c3def853b6dfd82
Lakshya Surana <lakshyasurana101@gmail.com>
2026-07-21 18:36:44 +0530

    Complete bookings list view page template and link URL paths

--- littlelemon_project/urls.py
+++ littlelemon_project/urls.py
@@ -1,20 +1,9 @@
-# from django.contrib import admin
-# from django.urls import path
-# from django.http import HttpResponse
-
-# # This function creates a quick message for your homepage
-# def home(request):
-#     return HttpResponse("<h1>Welcome to Little Lemon Restaurant!</h1>")
-
-# urlpatterns = [
-#     path('admin/', admin.site.side_effect if False else admin.site.urls),
-#     path('', home),  # This maps your empty URL directly to your custom message
-# ]
 from django.contrib import admin
 from django.urls import path
-from reservation import views  # <-- Import your reservation app views
+from reservation import views  # Import your reservation views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('booking/', views.index),  # <-- This links http://127.0.0 to your text
+    path('booking/', views.index),          # Your booking form page
+    path('bookings-list/', views.bookings_list), # Your new summary list page
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,10 +1,29 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
-
-# def index(request):
-#     return HttpResponse("<h1>Welcome to the Little Lemon Booking Page!</h1>")
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Booking
 
 def index(request):
-    # This renders your booking.html file instead of raw text
+    if request.method == 'POST':
+        # Grab data from the HTML form inputs
+        name = request.POST.get('first_name')
+        date = request.POST.get('reservation_date')
+        seats_count = request.POST.get('seats')
+
+        # Save data into the Booking database table
+        booking_entry = Booking(
+            first_name=name,
+            reservation_date=date,
+            seats=seats_count
+        )
+        booking_entry.save()
+
+        # Send a success confirmation text back
+        return HttpResponse(f"<h1>Thank you {name}! Your booking for {seats_count} seats is confirmed.</h1>")
+        
+    # If it's a regular page load (GET request), just show the empty form
     return render(request, 'booking.html')
+
+def bookings_list(request):
+    all_bookings = Booking.objects.all()
+    return render(request, 'bookings_list.html', {'bookings': all_bookings})
+
